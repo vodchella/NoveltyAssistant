a9b13872a8b9abecb22b5c9315a6b2d8720ee7f5
Twister <twister.kz@gmail.com>
2012-11-21 11:26:39 +0600

    Исправлен глюк, из-за которого не открывалась страница загрузок программы при проверке обновлений

--- qt_common.py
+++ qt_common.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from PyQt4.QtCore import Qt, QObject, pyqtSignal, pyqtSlot, SIGNAL, SLOT, QString, QDate, QTime
+from PyQt4.QtCore import Qt, QObject, pyqtSignal, pyqtSlot, SIGNAL, SLOT, QString, QDate, QTime, QUrl
 from PyQt4.QtCore import QRect, QVariant, QSize, QModelIndex, QAbstractListModel
 from PyQt4.QtGui import QApplication, QMessageBox, QDialog, QCursor, QPalette, QBrush, QColor
 from PyQt4.QtGui import QStyledItemDelegate, QPushButton, QListView, QAbstractItemView
 from PyQt4.QtGui import QScrollArea, QVBoxLayout, QWidget, QSpacerItem, QSizePolicy, QFrame
 from PyQt4.QtGui import QFont, QPlainTextEdit, QHBoxLayout, QSpinBox, QDialogButtonBox, QShortcut
 from PyQt4.QtGui import QLabel, QComboBox, QStackedWidget, QTableWidgetItem, QTableWidget
-from PyQt4.QtGui import QSystemTrayIcon, QMenu, QIcon, QPixmap, QTreeWidget, QTreeWidgetItem
+from PyQt4.QtGui import QSystemTrayIcon, QMenu, QIcon, QPixmap, QTreeWidget, QTreeWidgetItem, QDesktopServices
