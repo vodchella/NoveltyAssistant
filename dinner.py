99dac2f29480886c63f6b1e3173889e6ce522e15
Twister <twister.kz@gmail.com>
2013-02-12 15:21:10 +0600

    Ещё улучшен способ поиска меню на сайте dinner.kz, поуниверсален код

--- dinner.py
+++ dinner.py
@@ -13,13 +13,16 @@ def get_dinner_html():
     except:
         pass
 
+def arr_from_string(sz):
+    return ['%i. %s' % (i, a.strip()) for i, a in enumerate(sz.replace('2. ', '1. ').replace('3. ', '1. ').replace('4. ', '1. ').split('1. '))][1:]
+
 def go_deep(root, path_arr, result):
     for elem in filter(lambda l: l.tag == path_arr[0], iter(root)):
         if len(path_arr) > 1:
             go_deep(elem, path_arr[1:], result)
         else:
             if elem.text is not None:
-                result.append(elem.text.encode('utf-8').strip())
+                result += arr_from_string(elem.text_content().encode('utf-8'))
 
 def get_today_menu():
     arr = []
@@ -29,27 +32,19 @@ def get_today_menu():
             doc = lxml.html.document_fromstring(html)
             root_div = doc.xpath('/html/body/div[@id="wrapper"]/div[@id="body"]/div[@id="right"]/div[@id="booking"]')[0]
             
-#            paths = [
-#                'div/p',
-#                'div/div/strong/em/p/em',
-#                'div/div/strong/em',
-#                'div/div/p/em',
-#                'div/div/em/p/em'
-#            ]
+            #div/p
+            #div/p/em
+            #div/div/strong/em/p/em
+            #div/div/strong/em
+            #div/div/p/em
+            #div/div/em/p/em
+            
             paths = get_dinner_paths()
             for path in paths:
                 if not arr:
                     go_deep(root_div, path.split('/'), arr)
                 else:
                     break
-            
-            if not arr:
-                for divs in filter(lambda l: l.tag == 'div', iter(root_div)):
-                    for p in filter(lambda l: (l.tag == 'p'), iter(divs)):
-                        for em in filter(lambda l: (l.tag == 'em'), iter(p)):
-                            arr = ['%i. %s' % (i, a.strip()) for i, a in enumerate(em.text_content().encode('utf-8').replace('2. ', '1. ').replace('3. ', '1. ').replace('4. ', '1. ').split('1. '))][1:]
-                            break
-
         except:
             pass
     return arr
